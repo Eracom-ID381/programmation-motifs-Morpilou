0aa652fca0f424465be93dbad5a1e51915287e50
Morpilou <Morpilou@users.noreply.github.com>
2020-04-08 16:09:43 +0200

    Matrix

--- 2/sketch.js
+++ 2/sketch.js
@@ -2,18 +2,22 @@ function setup() {
   let navWidth = document.getElementById("nav").offsetWidth;
   let canvas = createCanvas(windowWidth - navWidth, windowHeight);
   canvas.parent('canvas');
-  background(255);
+  background(0);
+
 }
 
 function draw() {
-  fill(0);
-  for (let x = 10; x < width; x = x + 30) {
-    ellipse(x, height / 2, 10, 10);
+  let a = 20;
+  let b = 2;
+  noFill();
+  frameRate(random(2, 50));
+  clear();
+  background(0);
+  rectMode(CENTER);
+  for (var x = 0; x < width; x = x + random(0, 100)) {
+    stroke(color(random(0, 30), random(160, 255), random(0, 100)));
+    for (var y = 0; y < height; y = y + random(0, 50)) {
+      rect(x, y, random(0, 15), random(0, 15));
+    }
   }
-}
-
-function windowResized() {
-  let navWidth = document.getElementById("nav").offsetWidth;
-  resizeCanvas(windowWidth - navWidth, windowHeight);
-  background(255);
 }
